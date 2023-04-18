bf241a8deed313c26ecf15b71b4a87d781a7bf1d
Jeremy Cook <jeremy.cook@cloudacademy.com>
2023-04-18 22:37:20 +1200

    added GateKeeperSvc - contains secrets

---
+++ src/main/java/com/cloudacademy/banking/unsafe/GateKeeperSvc.java
@@ -0,0 +1,13 @@
+package com.cloudacademy.banking.unsafe;
+
+public class GateKeeperSvc {
+    /**
+     * The constant APP_KEY.
+     */
+    public final static String APP_KEY = "Z10N0101";
+    
+    /**
+     * The constant APP_SECRET.
+     */
+    public final static String APP_SECRET = "TrustNo1";
+}
