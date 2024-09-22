f922c69a52cf3322aa8300e1803663ee4f1af36d
Jeremy Cook <jeremy.cook@cloudacademy.com>
2024-09-23 10:32:15 +1200

    reordered modifiers

--- src/main/java/com/cloudacademy/banking/unsafe/GateKeeperSvc.java
+++ src/main/java/com/cloudacademy/banking/unsafe/GateKeeperSvc.java
@@ -4,16 +4,16 @@ public class GateKeeperSvc {
     /**
      * APP_KEY constant - secret key for the application
      */
-    public final static String APP_KEY = "Z10N0101";
+    public static final String APP_KEY = "Z10N0101";
 
-    public final static String PASSWORD = "password1234";
+    public static final String PASSWORD = "password1234";
 
     /**
      * APP_SECRET constant - secret for the application
      * Base64 encoded string for "TrustNo1"
      * v2
      */
-    public final static String APP_SECRET = "VHJ1c3RObzEK";
+    public static final String APP_SECRET = "VHJ1c3RObzEK";
 
-    public final static String AWS_SECRET_KEY = "c64e8c79aacf5ddb02f1274db2d973f363f4f553ab1692d8d203b4cc09692f797";
+    public static final String AWS_SECRET_KEY = "c64e8c79aacf5ddb02f1274db2d973f363f4f553ab1692d8d203b4cc09692f797";
 }
